3a1b1df73ca098055a4e66b1aace2c86f74d7192
esteban793 <lopreesteban@gmail.com>
2023-08-11 09:47:58 -0500

    primer tarea

---
+++ src/data/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title:'mi primer tarea',
+        description: 'esta es mi primer tarea'
+    },
+    {
+        id: 1,
+        title:'mi segunda tarea',
+        description: 'esta es mi segunda tarea'
+    },
+    {
+        id: 2,
+        title:'mi tercer tarea',
+        description: 'esta es mi primer tarea'
+    }
+]
